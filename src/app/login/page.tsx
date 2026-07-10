2eccf8f7180824e0478d1276e9fdd2214a5e0f8c
Sreenivasan <sreenivasanshanmugham@gmail.com>
2026-07-10 11:16:39 +0530

    feat(login): allow all to reach login page; validate license key on submit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,5 @@
-import { redirect } from "next/navigation";
-import { checkLicenseFromCookies, licenseRedirectTarget } from "@/lib/license-guard";
 import LoginClient from "./LoginClient";
 
-export const dynamic = "force-dynamic";
-
-export default async function LoginPage() {
-  const license = await checkLicenseFromCookies();
-  if (!license.allowLogin) {
-    redirect(licenseRedirectTarget(license));
-  }
+export default function LoginPage() {
   return <LoginClient />;
 }
